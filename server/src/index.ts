20b872700a123468b6abbbf74f1deddbc4aef24b
Seongho Lee <tjdgh2626@gmail.com>
2025-07-05 22:38:57 +0000

    setup: nestjs 패키지 설치 및 환경설정, fastify 패키지 제거

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,43 +1,5 @@
-import fastify from "fastify";
-import swagger from "@fastify/swagger";
-import swaggerUI from "@fastify/swagger-ui";
-
-import { pingRoutes } from "./infrastructure/routes/ping-pong";
-
-async function buildServer() {
-  const server = fastify({ logger: true });
-
-  server.register(swagger, {
-    openapi: {
-      info: {
-        title: "Linetracer Counter Server",
-        description: "ZETIN 라인트레이서 경연 대회 계수기 서버",
-        version: "0.0.1",
-      },
-      servers: [{ url: "http://localhost:3000" }],
-    },
-  });
-  server.register(swaggerUI, {
-    routePrefix: "/docs",
-    uiConfig: {
-      docExpansion: "list",
-    },
-  });
-
-  server.register(pingRoutes);
-
-  return server;
+async function main() {
+  console.log("Hello, world!");
 }
 
-async function startServer() {
-  const server = await buildServer();
-  try {
-    const res = await server.listen({ port: 3000 });
-    console.log(`server listening at ${res}`);
-  } catch (err) {
-    server.log.error(err);
-    process.exit(1);
-  }
-}
-
-startServer();
+main();
--- server/src/infrastructure/routes/ping-pong.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from "fastify";
-
-export async function pingRoutes(fastify: FastifyInstance) {
-  fastify.get("/ping", (request, reply) => {
-    reply.code(200).send("pong\n");
-  });
-}
