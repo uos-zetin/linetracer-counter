8c067441bacf7e4e1aaff0181e7b161433338df5
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2025-07-06 09:59:44 +0000

    feat: fetcher에 API Base URL 추가

--- client/src/shared/api/fetcher.ts
+++ client/src/shared/api/fetcher.ts
@@ -1,8 +1,10 @@
 import { ClientError, ServerError } from "./errors";
 
+const BASE_URL = process.env.API_BASE_URL || "http://localhost:3000/api";
+
 export async function fetcher<T>(url: string, options?: RequestInit): Promise<T> {
   try {
-    const response = await fetch(url, options);
+    const response = await fetch(BASE_URL + url, options);
     if (!response.ok) {
       if (response.status >= 500) {
         throw new ServerError(`Server error: ${response.status} - ${response.statusText}`);
