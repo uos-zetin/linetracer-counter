b7b00bf2d9981489e196af31170d2597742c9f05
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2025-07-08 02:22:05 +0000

    fix: React Component에 맞는 props 형태로 변환

--- client/src/entities/timer/ui/timer-view.tsx
+++ client/src/entities/timer/ui/timer-view.tsx
@@ -1,6 +1,11 @@
 import { formatMsToTime } from "./format-time";
 
-export function TimerView(remainingMs: number, timerStatus: "running" | "stopped" | "finished") {
+export type TimerViewProps = {
+  remainingMs: number;
+  timerStatus: "running" | "stopped" | "finished";
+};
+
+export function TimerView({ remainingMs, timerStatus }: TimerViewProps) {
   return (
     <div className="flex flex-col items-center justify-center h-full">
       <span className="text-4xl font-bold text-gray-800" title="Remaining Time">
