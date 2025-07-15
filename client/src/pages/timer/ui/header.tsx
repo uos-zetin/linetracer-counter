82a17012536dd911f11c03b9a8ae666a44d90d68
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2025-07-15 13:03:59 +0000

    feat: 헤더 컴포넌트 구현

---
+++ client/src/pages/timer/ui/header.tsx
@@ -0,0 +1,39 @@
+interface TimerViewHeaderProps {
+  type: "text" | "image";
+  content: string;
+  alt?: string;
+}
+
+export function TimerPageHeader({ type = "text", content, alt = "Competition Header" }: TimerViewHeaderProps) {
+  return (
+    <header
+      className="
+        sticky top-0 z-20 w-full
+        flex items-center justify-center text-center
+        bg-uos-primary-blue
+        text-white
+        "
+    >
+      {type === "text" ? (
+        <h1
+          className="
+            text-[5vw] py-4
+            font-bold text-center tracking-tight
+            aspect-banner
+          "
+        >
+          {content.toUpperCase()}
+        </h1>
+      ) : (
+        <img
+          src={content}
+          alt={alt}
+          className="
+            w-full h-auto
+            object-cover
+            "
+        />
+      )}
+    </header>
+  );
+}
