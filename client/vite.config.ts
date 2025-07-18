fc959c7f37d2a266644aca15b7d524d201c58fa5
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2025-07-18 13:08:16 +0000

    feat: CORS 문제 해결을 위한 proxy 설정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,4 +6,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
