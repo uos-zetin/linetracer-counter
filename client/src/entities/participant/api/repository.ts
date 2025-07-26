7b9d3c24a659735dfffa53b1f7801d8a998fa3b0
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2025-07-26 11:44:50 +0000

    feat: 누락된 타입 지정

--- client/src/entities/participant/api/repository.ts
+++ client/src/entities/participant/api/repository.ts
@@ -1,6 +1,7 @@
 import type { Fetcher } from "@/shared";
 import type { Participant } from "../model/types";
-import type { ParticipantRepository } from "./types";
+import type { ParticipantDto, ParticipantRepository } from "./types";
+import { parseParticipantDto } from "../lib/parse-dto";
 
 export class ParticipantFetcherRepository implements ParticipantRepository {
   private fetcher: Fetcher;
@@ -12,30 +13,30 @@ export class ParticipantFetcherRepository implements ParticipantRepository {
   }
 
   async getAllParticipants(divisionId: string): Promise<Participant[]> {
-    const response = await this.fetcher.get<Participant[]>(`/divisions/${divisionId}/participants`);
-    return response.data;
+    const response = await this.fetcher.get<ParticipantDto[]>(`/divisions/${divisionId}/participants`);
+    return response.data.map((dto) => parseParticipantDto(dto));
   }
 
   async getParticipantById(participantId: string): Promise<Participant | null> {
-    const response = await this.fetcher.get<Participant>(`/participants/${participantId}`);
-    return response.data ? response.data : null;
+    const response = await this.fetcher.get<ParticipantDto>(`/participants/${participantId}`);
+    return response.data ? parseParticipantDto(response.data) : null;
   }
 
   async createParticipant(
     divisionId: string,
     participant: Omit<Participant, "id" | "createdAt">,
   ): Promise<Participant> {
-    const response = await this.authFetcher.post<Participant>(`/divisions/${divisionId}/participants`, {
+    const response = await this.authFetcher.post<ParticipantDto>(`/divisions/${divisionId}/participants`, {
       body: participant,
     });
-    return response.data;
+    return parseParticipantDto(response.data);
   }
 
   async updateParticipant(participantId: string, participant: Participant): Promise<Participant | null> {
-    const response = await this.authFetcher.patch<Participant>(`/participants/${participantId}`, {
+    const response = await this.authFetcher.patch<ParticipantDto>(`/participants/${participantId}`, {
       body: participant,
     });
-    return response.data ? response.data : null;
+    return response.data ? parseParticipantDto(response.data) : null;
   }
 
   async deleteParticipant(participantId: string): Promise<void> {
