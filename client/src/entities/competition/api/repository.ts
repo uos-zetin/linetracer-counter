68c3b444740601df59f9d7c37211e0ca93c129b2
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2025-07-26 09:41:35 +0000

    feat: 대회 관련 Repository 구현

---
+++ client/src/entities/competition/api/repository.ts
@@ -0,0 +1,52 @@
+import type { Fetcher } from "@/shared";
+import type { Competition } from "../model/types";
+import type { CompetitionDto, CompetitionRepository } from "./types";
+import { parseCompetitionDto } from "../lib/parse-dto";
+
+export class CompetitionFetcherRepository implements CompetitionRepository {
+  private fetcher: Fetcher;
+  private authFetcher: Fetcher;
+
+  constructor(fetcher: Fetcher, authFetcher: Fetcher) {
+    this.fetcher = fetcher;
+    this.authFetcher = authFetcher;
+  }
+
+  async getAllCompetitions(): Promise<Competition[]> {
+    const response = await this.fetcher.get<CompetitionDto[]>("/competitions");
+
+    return response.data.map((competition) => parseCompetitionDto(competition));
+  }
+
+  async getCompetitionById(competitionId: string): Promise<Competition | null> {
+    const response = await this.fetcher.get<CompetitionDto>(`/competitions/${competitionId}`);
+
+    return response.data ? parseCompetitionDto(response.data) : null;
+  }
+
+  async createCompetition(competition: Competition): Promise<Competition> {
+    const response = await this.authFetcher.post<CompetitionDto>("/competitions", {
+      body: {
+        name: competition.name,
+        description: competition.description,
+      } as CompetitionDto,
+    });
+
+    return parseCompetitionDto(response.data);
+  }
+
+  async updateCompetition(competition: Competition): Promise<Competition> {
+    const response = await this.authFetcher.patch<CompetitionDto>(`/competitions/${competition.id}`, {
+      body: {
+        name: competition.name,
+        description: competition.description,
+      } as CompetitionDto,
+    });
+
+    return parseCompetitionDto(response.data);
+  }
+
+  async deleteCompetition(competitionId: string): Promise<void> {
+    await this.authFetcher.delete(`/competitions/${competitionId}`);
+  }
+}
