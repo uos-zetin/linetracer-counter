b5e2fb14cd4dae76656c365de8a90c925e6509b4
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2025-07-26 16:13:18 +0000

    feat: Public API 설정

--- client/src/entities/division/index.ts
+++ client/src/entities/division/index.ts
@@ -1,3 +1,4 @@
-export * from "./model/types";
-export * from "./lib/parse-dto";
 export * from "./api/types";
+export * from "./api/repository";
+export * from "./lib/parse-dto";
+export * from "./model/types";
