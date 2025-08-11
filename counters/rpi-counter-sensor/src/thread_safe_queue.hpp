aa2d967879a4a2d0b28382aa7e3cd2212818bd89
Seongho Lee <tjdgh2626@gmail.com>
2025-08-11 05:00:11 +0000

    refactor: ThreadSafeQueue 클래스의 복사 생성자 및 복사 할당 연산자 삭제, 이동 생성자 및 이동 할당 연산자 허용

--- counters/rpi-counter-sensor/src/thread_safe_queue.hpp
+++ counters/rpi-counter-sensor/src/thread_safe_queue.hpp
@@ -10,7 +10,7 @@ template <typename T>
 class ThreadSafeQueue {
 private:
     std::queue<T> queue_;
-    std::mutex mutex_;
+    mutable std::mutex mutex_;
     std::condition_variable cv_;
     size_t max_size_;
 
@@ -18,22 +18,13 @@ public:
     ThreadSafeQueue() : max_size_(std::numeric_limits<size_t>::max()) {}
     explicit ThreadSafeQueue(size_t max_size) : max_size_(max_size) {}
 
-    ThreadSafeQueue(const ThreadSafeQueue& other) {
-        std::lock_guard<std::mutex> lock(const_cast<std::mutex&>(other.mutex_));
-        queue_ = other.queue_;
-        max_size_ = other.max_size_;
-    }
+    // 복사 생성자와 복사 할당 연산자 삭제
+    ThreadSafeQueue(const ThreadSafeQueue&) = delete;
+    ThreadSafeQueue& operator=(const ThreadSafeQueue&) = delete;
 
-    ThreadSafeQueue& operator=(const ThreadSafeQueue& other) {
-        if (this != &other) {
-            std::lock(mutex_, const_cast<std::mutex&>(other.mutex_));
-            std::lock_guard<std::mutex> lock_this(mutex_, std::adopt_lock);
-            std::lock_guard<std::mutex> lock_other(const_cast<std::mutex&>(other.mutex_), std::adopt_lock);
-            queue_ = other.queue_;
-            max_size_ = other.max_size_;
-        }
-        return *this;
-    }
+    // 이동 생성자와 이동 할당 연산자 허용
+    ThreadSafeQueue(ThreadSafeQueue&&) = default;
+    ThreadSafeQueue& operator=(ThreadSafeQueue&&) = default;
 
     void push(const T& data) {
         std::lock_guard<std::mutex> lock(mutex_);
@@ -83,17 +74,17 @@ public:
     }
 
     bool empty() const {
-        std::lock_guard<std::mutex> lock(const_cast<std::mutex&>(mutex_));
+        std::lock_guard<std::mutex> lock(mutex_);
         return queue_.empty();
     }
 
     bool full() const {
-        std::lock_guard<std::mutex> lock(const_cast<std::mutex&>(mutex_));
+        std::lock_guard<std::mutex> lock(mutex_);
         return queue_.size() >= max_size_;
     }
 
     size_t size() const {
-        std::lock_guard<std::mutex> lock(const_cast<std::mutex&>(mutex_));
+        std::lock_guard<std::mutex> lock(mutex_);
         return queue_.size();
     }
 
