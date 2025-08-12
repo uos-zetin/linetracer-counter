2a093517059c129c0cb522bc4c2f7123268e1b27
Seongho Lee <tjdgh2626@gmail.com>
2025-08-12 12:04:50 +0000

    feat: Queue 개선, 센서 데이터 읽기 및 전송 로직 개선

--- counters/rpi-counter-sensor/src/thread_safe_queue.hpp
+++ counters/rpi-counter-sensor/src/thread_safe_queue.hpp
@@ -1,94 +1,53 @@
-#pragma once
-
-#include <queue>
 #include <mutex>
+#include <queue>
 #include <condition_variable>
-#include <limits>
-#include <chrono>
+#include <vector>
 
-template <typename T>
+template<typename T>
 class ThreadSafeQueue {
 private:
+    mutable std::mutex mtx_;
     std::queue<T> queue_;
-    mutable std::mutex mutex_;
-    std::condition_variable cv_;
-    size_t max_size_;
+    std::condition_variable condition_;
 
 public:
-    ThreadSafeQueue() : max_size_(std::numeric_limits<size_t>::max()) {}
-    explicit ThreadSafeQueue(size_t max_size) : max_size_(max_size) {}
-
-    // 복사 생성자와 복사 할당 연산자 삭제
+    ThreadSafeQueue() = default;
     ThreadSafeQueue(const ThreadSafeQueue&) = delete;
     ThreadSafeQueue& operator=(const ThreadSafeQueue&) = delete;
 
-    // 이동 생성자와 이동 할당 연산자 허용
-    ThreadSafeQueue(ThreadSafeQueue&&) = default;
-    ThreadSafeQueue& operator=(ThreadSafeQueue&&) = default;
-
-    void push(const T& data) {
-        std::lock_guard<std::mutex> lock(mutex_);
-        if (queue_.size() >= max_size_) {
-            queue_.pop();
-        }
-        queue_.push(data);
-        cv_.notify_one();
+    void push(const T& item) {
+        std::lock_guard<std::mutex> lock(mtx_);
+        queue_.push(item);
+        condition_.notify_one();
     }
 
-    void push(T&& data) {
-        std::lock_guard<std::mutex> lock(mutex_);
-        if (queue_.size() >= max_size_) {
-            queue_.pop();
-        }
-        queue_.push(std::move(data));
-        cv_.notify_one();
-    }
-
-    bool try_pop(T& data) {
-        std::lock_guard<std::mutex> lock(mutex_);
-        if (queue_.empty()) {
+    bool try_pop(T& item) {
+        std::lock_guard<std::mutex> lock(mtx_);
+        if(queue_.empty()) {
             return false;
         }
-        data = std::move(queue_.front());
+        item = queue_.front();
         queue_.pop();
         return true;
     }
 
-    T pop() {
-        std::unique_lock<std::mutex> lock(mutex_);
-        cv_.wait(lock, [this] { return !queue_.empty(); });
-        T data = std::move(queue_.front());
-        queue_.pop();
-        return data;
-    }
-
-    template<typename Rep, typename Period>
-    bool pop(T& data, const std::chrono::duration<Rep, Period>& timeout) {
-        std::unique_lock<std::mutex> lock(mutex_);
-        if (cv_.wait_for(lock, timeout, [this] { return !queue_.empty(); })) {
-            data = std::move(queue_.front());
+    std::vector<T> pop_all() {
+        std::lock_guard<std::mutex> lock(mtx_);
+        std::vector<T> result;
+        while (!queue_.empty()) {
+            result.push_back(queue_.front());
             queue_.pop();
-            return true;
         }
-        return false;
-    }
-
-    bool empty() const {
-        std::lock_guard<std::mutex> lock(mutex_);
-        return queue_.empty();
-    }
-
-    bool full() const {
-        std::lock_guard<std::mutex> lock(mutex_);
-        return queue_.size() >= max_size_;
+        return result;
     }
 
     size_t size() const {
-        std::lock_guard<std::mutex> lock(mutex_);
+        std::lock_guard<std::mutex> lock(mtx_);
         return queue_.size();
     }
 
-    size_t max_size() const {
-        return max_size_;
+    bool empty() const {
+        std::lock_guard<std::mutex> lock(mtx_);
+        return queue_.empty();
     }
 };
